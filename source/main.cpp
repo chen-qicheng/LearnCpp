13806835457f98211604b2dbd6d390b9a9552978
chen-qicheng <1530428361@qq.com>
2024-02-05 14:26:57 +0800

    1、调整工程目录结构，仓库内储用于存放LeanCpp单一工程，结构更加合理。2、增加gtest相关组件

---
+++ source/main.cpp
@@ -0,0 +1,47 @@
+#include <unistd.h> //Unix Standard 
+#include <stdio.h> //Standard Input Output
+#include <glog/logging.h>
+
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+
+
+int main (int argc, char **argv)
+{
+    google::InitGoogleLogging(argv[0]);//使用glog之前必须先初始化库，仅需执�?一次，�?��内为程序�?
+    FLAGS_alsologtostderr = true;     //�?��将日志输出到文件和stderr
+    FLAGS_colorlogtostderr = true;    //�?���?��不同颜色显示
+    FLAGS_log_dir = "../logs"; //
+    // LOG(INFO) << "info";
+    // LOG(WARNING) << "warning";
+    // LOG(ERROR) << "error";
+    /*LOG(FATAL) << "fatal";*/
+
+
+    char buff1[1024];
+    char buff2[1024];
+    int ret;
+
+    int fd1 = open("./test", O_RDWR|O_TRUNC);
+    if(fd1 == -1)
+    {
+        printf("open ./test fail!\n");
+        LOG(ERROR) << "open ./test fail!";
+        return fd1;
+    }
+    ret = lseek(fd1, 0, SEEK_END);
+
+    sprintf(buff1,"try to write the file");
+    ret = write(fd1, buff1,sizeof(buff1));
+
+    ret = lseek(fd1, 0, SEEK_SET);
+    ret = read(fd1, buff2, sizeof(buff2));
+    printf("read:%s count:%d\n", buff2, ret);
+    LOG(INFO) << buff2;
+
+    ret = close(fd1);
+
+    google::ShutdownGoogleLogging();  //当�?结束glog时必须关�?��，否则会内存溢出
+    return 0;
+}
